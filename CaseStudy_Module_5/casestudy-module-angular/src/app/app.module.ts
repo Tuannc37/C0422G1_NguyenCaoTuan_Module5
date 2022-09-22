04c4c3568e1b9f37747f3cb6eda1b464333ac882
caotuan <caotuan1994hk@gmail.com>
2022-09-22 16:57:20 +0700

    update

---
+++ CaseStudy_Module_5/casestudy-module-angular/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FacilityComponent } from './facility/facility.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FacilityComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
